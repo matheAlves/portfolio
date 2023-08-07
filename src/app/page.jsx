772e77557ceaeff351833ad698e016107359f3c0
Matheus S Alves <matheus.souza.alves@gmail.com>
2023-08-07 12:17:26 -0300

    feat: changes in scroll effects

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,30 +6,40 @@ import './page.css'
 import { useEffect } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { Flip } from 'gsap/Flip'
 
 export default function Home() {
   gsap.registerPlugin(ScrollTrigger)
   const lenis = useLenis(({ scroll }) => {
     console.log('hey')
   })
-
+  
   gsap.to(".main-image", {
     x: -600,
     opacity: 0,
     ease: "power4.inOut",
     scrollTrigger: {
-      markers: {startColor: "green", endColor: "red", fontSize: "12px"},
-      trigger: ".main-image",
-      start: "top center",
-      // stop: "top top",
-      // pin: true,
-      scrub: 1,
+      trigger: ".nav-bar",
+      start: "top top",
+      scrub: 2,
     },
   })
 
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, )
+  gsap.to(".nav-bar", {
+    y: -400,
+    scrollTrigger: {
+      toggleClass: "sticky",
+      trigger: ".nav-bar",
+      start: "-1 top",
+      end: "+=2000",
+      scrub: 2,
+
+    },
+  })
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0)
+  // },)
 
   return (
     <ReactLenis
@@ -44,80 +54,85 @@ export default function Home() {
         className='flex min-h-screen flex-col fade-in-from-left justify-center switch'
       >
         <section
-          className="flex flex-col lg:flex-row items-center justify-around p-20 section-1 h-screen"
+        className='bg-blue-400'
         >
           <div
-            className='text-center  mb-10'
-          >
-            <h1
-              className='text-5xl mb-10 font-sans'>
-              Matheus Alves
-            </h1>
-            <p className={`m-0 max-w-[30ch] text-base opacity-50`}>
-              full stack developer.
-            </p>
-          </div>
-
-          <Image
-            className="relative main-image ml-20"
-            src="/m.png"
-            alt="matheus"
-            width={450}
-            height={0}
-            priority
-          />
-
-          <div className="mb-32 text-center lg:grid lg:mb-0 lg:grid-cols-2 lg:text-left fade-in-from-left-delayed">
-            <a
-              href="https://github.com/matheAlves"
-              className="group px-5 py-9 hover:underline decoration-wavy decoration-4 underline-offset-8 decoration-lime-400"
-              target="_blank"
-              rel="noopener noreferrer"
+            className='mg-80 nav-bar flex flex-col lg:flex-row items-center justify-around p-20 h-screen '>
+            <div
+              className='text-center mb-10 name'
             >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                github{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none fade-in-from-left-delayed">
-                  -&gt;
-                </span>
-              </h2>
-            </a>
+              <h1
+                className='text-5xl mb-10 font-sans'>
+                Matheus Alves
+              </h1>
+              <p className={`m-0 max-w-[30ch] text-base opacity-50`}>
+                full stack developer.
+              </p>
+            </div>
 
-            <a
-              href="https://www.linkedin.com/in/mathealves/"
-              className="group px-5 py-9 hover:underline decoration-solid decoration-4 underline-offset-8 decoration-cyan-400"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                linkedin{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none fade-in-from-left-delayed">
-                  -&gt;
-                </span>
-              </h2>
-            </a>
+            <Image
+              className="main-image"
+              src="/m.png"
+              alt="matheus"
+              width={450}
+              height={0}
+              priority
+            />
+
+            <div className="mb-32 text-center lg:grid lg:mb-0 lg:grid-cols-2 lg:text-left fade-in-from-left-delayed links">
+              <a
+                href="https://github.com/matheAlves"
+                className="group px-5 py-9 hover:underline decoration-wavy decoration-4 underline-offset-8 decoration-lime-400"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                  github{' '}
+                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none fade-in-from-left-delayed">
+                    -&gt;
+                  </span>
+                </h2>
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/mathealves/"
+                className="group px-5 py-9 hover:underline decoration-solid decoration-4 underline-offset-8 decoration-cyan-400"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                  linkedin{' '}
+                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none fade-in-from-left-delayed">
+                    -&gt;
+                  </span>
+                </h2>
+              </a>
+            </div>
           </div>
         </section>
 
         <section
-          className='h-screen'
+          className='h-screen bg-purple-400'
         >
-
-
-
-        </section>
-        <section
-          className='flex h-screen justify-center content-end'
-        >
-
-
           <p className={`m-0 max-w-[30ch] text-base opacity-50 self-center v-`}>
-            em construção. © 2023
+            2
           </p>
         </section>
-
+        <section
+          className='flex h-screen justify-center bg-emerald-400'
+        >
+          <p className={`m-0 max-w-[30ch] text-base opacity-50 self-center v-`}>
+            3
+          </p>
+        </section>
+        <section
+          className='flex h-screen justify-center bg-blue-400 content-end'
+        >
+          <footer className={`m-0 max-w-[30ch] text-base opacity-50 self-center v- footer`}>
+            em construção. © 2023
+          </footer>
+        </section>
       </main>
-
     </ReactLenis>
-
   )
 }
